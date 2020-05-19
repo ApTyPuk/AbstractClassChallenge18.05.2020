a9bda38f5be1b86fd2fc9410b7b33deef6b3b5b6
PrIvATe <kpackalv@yahoo.com>
2020-05-19 22:43:27 +0300

    Commit 19/05/2020

--- src/com/Artur/LinkedList.java
+++ src/com/Artur/LinkedList.java
@@ -4,12 +4,15 @@ package com.Artur;
 import java.util.ArrayList;
 
 public class LinkedList extends ListItem {
+public static ArrayList<ListItem>listItem = new ArrayList<>();
 
     public LinkedList(String parameter) {
         super(parameter);
     }
 
-    ArrayList<ListItem> listItem = new ArrayList<>();
+    public ArrayList<ListItem> getListItem() {
+        return listItem;
+    }
 
     @Override
     public void nextItem() {
@@ -21,18 +24,42 @@ public class LinkedList extends ListItem {
         super.previousItem();
     }
 
-    public void addToList(ListItem item){
+    @Override
+    public int compareTo(ListItem listItem) {
+        return super.compareTo(listItem);
+    }
 
-        if(listItem.isEmpty()){
-            listItem.add(item);
-            super.setNextItem(item);
-            super.setPreviousItem(item);
-        }
+    public void addToList(String name){
+        LinkedList item = new LinkedList(name);
 
-        for (int i = 0; i < listItem.size(); i++) {
+        if(!listItem.contains(item)){
 
-            if (listItem.get(i).compareTo(item) < 0){
-                
+            if(listItem.get(0) == null){
+                listItem.add(0, item);
+                super.setNextItem(item);
+                super.setPreviousItem(item);
+            }
+
+            if (item.compareTo(listItem.get(0)) < 0){
+                listItem.add(0, item);
+                super.setNextItem(listItem.get(1));
+                super.setPreviousItem(listItem.get(0));
+
+            }
+
+            for (int i = 0; i < listItem.size(); i++) {
+
+                if (item.compareTo(listItem.get(i)) > 0){
+                    if(item.compareTo(listItem.get(i+1)) < 0)
+                        listItem.add((i+1), item);
+                    super.setNextItem(listItem.get(i+2));
+                    super.setPreviousItem(listItem.get(i));
+                }else if((item.compareTo(listItem.get(i)) > 0) && (listItem.get(i+1) == null)){
+                    listItem.add((i+1), item);
+                    super.setNextItem(null);
+                    super.setPreviousItem(listItem.get(i));
+                }
+
             }
 
         }
@@ -40,6 +67,32 @@ public class LinkedList extends ListItem {
     }
 
 
+    public static void print(){
+        for(int i = 0; i < listItem.size(); i++){
+//        for(ListItem i: listItem){
+            System.out.println(listItem.get(i).getParameter());
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        addToList("c");
+        LinkedList one = new LinkedList("c");
+        LinkedList two = new LinkedList("b");
+        LinkedList three = new LinkedList("d");
+        LinkedList four = new LinkedList("f");
+        LinkedList five = new LinkedList("a");
+
+        listItem.addToList(one);
+        LinkedList.listItem.add(two);
+        LinkedList.listItem.add(three);
+        LinkedList.listItem.add(four);
+        LinkedList.listItem.add(five);
+
+
+    }
 
 
 
--- src/com/Artur/Main.java
+++ src/com/Artur/Main.java
@@ -3,9 +3,7 @@ package com.Artur;
 public class Main {
 
     public static void main(String[] args) {
-	String a = "a";
-	String b = "b";
-        System.out.println(b.compareTo(a));
+
 
     }
 }
