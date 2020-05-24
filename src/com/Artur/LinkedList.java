10f3098fd5e70f3a6171597cf8a82967f6792856
PrIvATe <kpackalv@yahoo.com>
2020-05-24 22:23:36 +0300

    Commit 24/05/2020 Finally now it's working!

--- src/com/Artur/LinkedList.java
+++ src/com/Artur/LinkedList.java
@@ -1,7 +1,6 @@
 package com.Artur;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class LinkedList extends ListItem {
@@ -9,210 +8,167 @@ public class LinkedList extends ListItem {
 public static ArrayList<LinkedList> listItem = new ArrayList<>();
     public static int currPos = 0;
 
-
-    public LinkedList(String parameter) {
-        super(parameter);
-    }
-
-    public static void nextItem() {
-        try {
-            LinkedList nextPos = listItem.get(currPos + 1);
-            if (nextPos != null) {
-//            setNextItem(nextPos);
-                currPos++;
-                System.out.println(nextPos.getValue());
-            }
-        }catch (Exception e) {
-            System.out.println("You've reached end of list.");
-        }
-    }
-
-
-    public static void previousItem() {
-        try {
-            LinkedList prevPos = listItem.get(currPos - 1);
-            if (prevPos != null) {
-//            setPreviousItem(prevPos);
-                currPos--;
-                System.out.println(prevPos.getValue());
-            }
-        }catch(Exception e) {
-            System.out.println("You've reached start of list.");
-        }
+    public LinkedList(String value) {
+        super(value);
     }
 
     @Override
     ListItem next() {
-        return null;
+        if(hasNext(currPos+1)){
+            setNext(listItem.get(currPos + 1));
+            setPrevious(listItem.get(currPos));
+            currPos++;
+            System.out.println(super.nextItem.getValue());
+            return super.nextItem;
+        }
+        System.out.println("You've reached end of list.");
+        return listItem.get(currPos);
     }
 
     @Override
     ListItem setNext(ListItem item) {
-        return null;
+        super.nextItem = item;
+        return super.nextItem;
     }
 
     @Override
     ListItem previous() {
-        return null;
+        if(hasNext(currPos-1)){
+            setPrevious(listItem.get(currPos - 1));
+            setNext(listItem.get(currPos));
+            currPos--;
+            System.out.println(super.previousItem.getValue());
+            return super.previousItem;
+        }
+        System.out.println("You've reached start of list.");
+        return listItem.get(currPos);
     }
+
 
     @Override
     ListItem setPrevious(ListItem item) {
-        return null;
+        super.previousItem = item;
+        return super.previousItem;
     }
 
     @Override
     int compareTo(ListItem item) {
-//        String temp = getValue().toString();
-//        System.out.println(temp);
-//        System.out.println(item.getValue().toString());
         return getValue().toString().compareTo(item.getValue().toString());
     }
 
-//    public static void addToList(String name){
-//        LinkedList tempList = new LinkedList(name);
-//        listItem.add(tempList);
-//    }
+    private static boolean hasNext(int index){
+        try {
+            listItem.get(index);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+
+    }
 
     public void addToList(){
         LinkedList item = new LinkedList(getValue().toString());
 
+        if(listItem.isEmpty()){
+            listItem.add(item);
+//            System.out.println("\nAdding (0): " + item.getValue().toString());
+        }
 
-//        if(!listItem.contains(item)){
+        if ((item.compareTo(listItem.get(0)) < 0) && (!listItem.contains(item))){
+            listItem.add(0, item);
+//            System.out.println("Adding (1): " + item.getValue().toString());
+        }
 
-            if(listItem.isEmpty()){
-                listItem.add(item);
-                System.out.println("\nAdding (0): " + item.getValue().toString());
-//                super.setNextItem(item);
-//                super.setPreviousItem(item);
-            }
-
-            if (item.compareTo(listItem.get(0)) < 0){
-                listItem.add(0, item);
-                System.out.println("Adding (1): " + item.getValue().toString());
-
-//                super.setNextItem(listItem.get(1));
-//                super.setPreviousItem(listItem.get(0));
-
-            }
-
-            for (int i = 0; i < listItem.size(); i++) {
-
-                try{
-                    int getI = item.compareTo(listItem.get(i));
-                    int getIplus1 = item.compareTo(listItem.get(i+1));
-
-                    if (getI > 0){
-                        System.out.println("Skipping here :" +item.getValue().toString() );
-                    }
-                    if ((getI > 0) && (getIplus1 < 0)){
-                        listItem.add((i+1), item);
-                        System.out.println("Adding (2): " + item.getValue().toString());
-
-    //                    super.setNextItem(listItem.get(i+2));
-    //                    super.setPreviousItem(listItem.get(i));
-                    }
-
-                    System.out.println(i+"----->" + listItem.size());
-                    System.out.println(i+"----->" + listItem.get(listItem.size()-1).getValue());
-                    System.out.println(listItem.get(i) == listItem.get(listItem.size()-1));
-
-                    if(  (getI > 0) && (listItem.get(i) == listItem.get(listItem.size()-1))  ){
-//                        LinkedList temp = listItem.get(i);
-//                        listItem.add(i, item);
-//                        listItem.set(i, temp);
-//                        listItem.add((i), item);
-                        System.out.println("Adding (3): " + item.getValue().toString());
-
-    //                    super.setNextItem(null);
-    //                    super.setPreviousItem(listItem.get(i));
-                    }
-                }catch (Exception e){
+        for (int i = 0; i < listItem.size(); i++) {
+            if(!listItem.contains(item)) {
+                if (  (item.compareTo(listItem.get(i)) > 0) && (!hasNext(i + 1))  ) {
+                    listItem.add(item);
+//                    System.out.println("Adding (3): " + item.getValue().toString());
                 }
 
-                System.out.println("==================================");
+                if ( (hasNext(i + 1)) && (item.compareTo(listItem.get(i)) > 0) && (item.compareTo(listItem.get(i+1)) < 0)) {
+                    listItem.add((i + 1), item);
+//                    System.out.println("Adding (2): " + item.getValue().toString());
+                }
             }
-
-//        }
-
-    }
-
-
-    public static void print(){
-        for(int i = 0; i < listItem.size(); i++){
-//        for(ListItem i: listItem){
-            System.out.println(listItem.get(i).getValue());
         }
     }
+
+
 
 
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        String stringData = "g w e z c d b a";
+        String stringData = "g g w e z c w d b a";
         String[] data = stringData.split(" ");
-        System.out.println("String Array 'data' size is: " + data.length);
-        System.out.println("String Array 'data' values are: " + Arrays.toString(data));
-
-//        LinkedList aa = new LinkedList("a");
-//        LinkedList bb = new LinkedList("b");
-//        System.out.println(aa.compareTo(bb));
+//        System.out.println("String Array 'data' size is: " + data.length);
+//        System.out.println("String Array 'data' values are: " + Arrays.toString(data));
 
         for (String s : data){
             LinkedList list = new LinkedList(s);
             list.addToList();
         }
 
-        System.out.println("\nSize of listItem array: "+ listItem.size());
-
+//        System.out.println("\nSize of listItem array: "+ listItem.size());
 
         for(LinkedList l : listItem) {
             System.out.println(l.getValue().toString());
         }
 
+        System.out.println(hasNext(8));
 
+        boolean quit = false;
+        int action;
 
-//        boolean quit = false;
-//        int action;
-//
-//        System.out.println("\nNow showing position: '"+ (currPos) +"' with value inside: '"+ listItem.get(currPos).getValue()+"'." );
-//
-//        while(!quit){
-//            System.out.print("\nChoose your action: ");
-//            action = scanner.nextInt();
-//            scanner.nextLine();
-//            switch(action){
-//                case 0:
-//                    System.out.println("\nShutting down...");
-//                    quit = true;
-//                    break;
-//                case 1:
-//                    System.out.print("\nPrevious item: ");
-//                    previousItem();
-//                    break;
-//                case 2:
-//                    System.out.print("\nNext item: ");
-//                    nextItem();
-//                    break;
-//                case 3:
-//                    System.out.println("\nPrinting items: ");
-//                    print();
-//                    break;
-//                case 4:
-//                    System.out.println("\nDeleting your item!");
-//                    delete();
-//                    break;
-//
-//            }
-//        }
+        System.out.println("\nNow showing position: '"+ (currPos) +"' with value inside: '"+ listItem.get(currPos).getValue()+"'." );
+
+        while(!quit){
+            System.out.print("\nChoose your action: ");
+            action = scanner.nextInt();
+            scanner.nextLine();
+            switch(action){
+                case 0:
+                    System.out.println("\nShutting down...");
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("\nPrevious item: ");
+                    listItem.get(currPos).previous();
+                    break;
+                case 2:
+                    System.out.print("\nNext item: ");
+                    listItem.get(currPos).next();
+                    break;
+                case 3:
+                    System.out.println("\nPrinting items: ");
+                    print();
+                    break;
+                case 4:
+                    System.out.println("\nDeleting your item!");
+                    delete();
+                    break;
+
+            }
+        }
 
     }
 
     private static void delete(){
         System.out.println("Removing: '"+ (currPos) +"' with value inside: '"+ listItem.get(currPos).getValue()+"'." );
         listItem.remove(currPos);
+        if(currPos != 0){
+            currPos--;
+        }
         System.out.println("\nNow showing position: '"+ (currPos) +"' with value inside: '"+ listItem.get(currPos).getValue()+"'." );
     }
+
+    public static void print(){
+        for (LinkedList i : listItem) {
+            System.out.println(i.getValue());
+        }
+    }
+
 
 
 
--- src/com/Artur/Main.java
+++ src/com/Artur/Main.java
@@ -1,9 +1,51 @@
 package com.Artur;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+    public static ArrayList<String> intArray = new ArrayList<>();
     public static void main(String[] args) {
+
+        intArray.add("1");
+        intArray.add("2");
+        intArray.add("3");
+        intArray.add("4");
+        intArray.add("5");
+
+        System.out.println(intArray.toString());
+
+        addToArray("6");
+        addToArray("4");
+
+
+        System.out.println(intArray.toString());
 
 
     }
-}
+
+
+        public static void addToArray(String string){
+            for (int i = 0; i<intArray.size(); i++ ){
+                if(string.compareTo(intArray.get(i)) > 0 && (!hasNext(i + 1))){
+                    intArray.add(string);
+                    System.out.println("Adding (3): " + string);
+                }
+
+            }
+        }
+
+
+        private static boolean hasNext(int index){
+            try {
+                if(intArray.get(index) != null) {
+                    return true;
+                }
+            }catch (Exception e){
+            }
+            return false;
+        }
+
+
+
+
+    }
